7be66d92efbb9ca7fab32e7b585ca3d123cc8348
ImDen <neckromant90@gmail.com>
2019-10-07 23:08:24 +0300

    адаптив header

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let burgerBtn = document.querySelector('.burger__item');
+let navItem   = document.querySelectorAll('.nav__item');
+let burgerItem   = document.querySelector('.burger__btn');
+
+
+burgerBtn.addEventListener('click', () => {
+  burgerItem.classList.toggle('active');
+  navItem.forEach(elem => {
+    elem.style.display = (elem.style.display == "block") ? "none" : "block"
+  })
+});
