b32e6fa520f60a5196482bf8ab5781c808457e6c
ImDen <neckromant90@gmail.com>
2019-10-08 18:09:39 +0300

    Адаптив

--- js/catalog-main.js
+++ js/catalog-main.js
@@ -1,38 +1,41 @@
 'use strict'
 
-btnActive();
+window.onload = () => {
 
-function btnActive() {
-  var btns = document.getElementsByClassName('products__items');
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener('click', function () {
-      var current = document.getElementsByClassName('active');
-      current[0].classList.remove('active')
-      this.classList.add('active');
-      var attr = this.getAttribute('data-target');
-      if(attr == 'all'){
-        showCard()
-      }else{
-        showCard(),
-        hideCard(attr)
-      };
-    });
-  }
-}
+  btnActive();
 
-function hideCard(card) {
-  var cards = document.getElementsByClassName('card');
-  for (var i = 0; i < cards.length; i++) {
-    var attr = cards[i].getAttribute('data-card');
-    if(card != attr){
-      cards[i].classList.add('hide')
+  function btnActive() {
+    var btns = document.getElementsByClassName('products__items');
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener('click', function () {
+        var current = document.getElementsByClassName('active');
+        current[0].classList.remove('active')
+        this.classList.add('active');
+        var attr = this.getAttribute('data-target');
+        if(attr == 'all'){
+          showCard()
+        }else{
+          showCard(),
+          hideCard(attr)
+        };
+      });
     }
   }
-}
-function showCard() {
-  var cards = document.getElementsByClassName('card');
-  for (var x = 0; x < cards.length; x++) {
-    cards[x].classList.remove('hide')
-  }
-}
 
+  function hideCard(card) {
+    var cards = document.getElementsByClassName('card');
+    for (var i = 0; i < cards.length; i++) {
+      var attr = cards[i].getAttribute('data-card');
+      if(card != attr){
+        cards[i].classList.add('hide')
+      }
+    }
+  }
+  function showCard() {
+    var cards = document.getElementsByClassName('card');
+    for (var x = 0; x < cards.length; x++) {
+      cards[x].classList.remove('hide')
+    }
+  }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-let burgerBtn = document.querySelector('.burger__item');
-let navItem   = document.querySelectorAll('.nav__item');
-let burgerItem   = document.querySelector('.burger__btn');
-
-
-burgerBtn.addEventListener('click', () => {
-  burgerItem.classList.toggle('active');
-  navItem.forEach(elem => {
-    elem.style.display = (elem.style.display == "block") ? "none" : "block"
-  })
-});
+window.onload = () => {
+  let burgerBtn = document.querySelector('.burger__item');
+  let navItem   = document.querySelectorAll('.nav__item');
+  let burgerItem   = document.querySelector('.burger__btn');
+  
+  
+  burgerBtn.addEventListener('click', () => {
+    burgerItem.classList.toggle('active');
+    navItem.forEach(elem => {
+      elem.style.display = (elem.style.display == "block") ? "none" : "block"
+    })
+  });
+}
