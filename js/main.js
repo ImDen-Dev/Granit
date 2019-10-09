d7d5cbfeabb7a2fe28a65162e868a26a35b36590
ImDen <neckromant90@gmail.com>
2019-10-09 16:03:37 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ window.onload = () => {
   let modalBtn = document.querySelectorAll('button[type=button]');
   let btns = document.querySelectorAll('.products__items');
   let cards = document.querySelectorAll('.card');
+  // let closeWrapper = document.querySelector('.modal__wrapper');
+
   
   burgerBtn.addEventListener('click', () => {
     burgerItem.classList.toggle('active');
@@ -19,7 +21,8 @@ window.onload = () => {
 
     element.addEventListener('click', () => {
       let cardTitle = element.parentNode.querySelector('h4');
-      modal.style.display = 'block';
+      // modal.style.display = 'block';
+      fadeIn(modal)
       
       if(cardTitle){
         let text = cardTitle.textContent;
@@ -32,10 +35,50 @@ window.onload = () => {
     let closeModal = modal.querySelector('.modal__close');
     
     closeModal.addEventListener('click', () => {
-      modal.style.display = 'none';
+      fadeOut(modal);
     });
+
+    let closeWrapper = modal.querySelector('.modal__wrapper');
+    
+    closeWrapper.addEventListener('click', e => {
+      if (e.target.closest('.modal__wrapper') && !e.target.closest('.modal__window'))
+      {
+        fadeOut(modal);
+      }
+    });
+
   });
 
+
+  function fadeOut(element) {
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 5);
+  }
+
+  function fadeIn(element) {
+    var op = 0.1;
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 5);
+  }
+
+
+
+// card selection
   btns.forEach(btn =>{
     btn.addEventListener('click', () => {
       let current = document.querySelector('.active');
@@ -58,7 +101,6 @@ window.onload = () => {
       c.classList.remove('hide')
     });
   };
+// card selection
 
-  console.log((window.location.pathname == '/Catalog.html') ? 'yes' : 'no')
-  console.log(window.location.pathname)
 }
